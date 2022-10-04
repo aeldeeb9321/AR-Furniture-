15f4e36e74e0957dcdf60b56e70eec866f78933a
Ali Eldeeb <aeldeeb@fordham.edu>
2022-10-03 22:07:31 -0700

    Feat: Added a collection view at the bottom of the view for furniture items and added a tapGestureRecognizer

--- AR Furniture/AR Furniture/ViewController.swift
+++ AR Furniture/AR Furniture/ViewController.swift
@@ -8,12 +8,13 @@
 import UIKit
 import ARKit
 import RealityKit
-
+import Combine
 class ViewController: UIViewController {
     
     //MARK: - Properties
     let itemsArray: [String] = ["cup", "vase", "boxing", "table"]
     var selectedItem: String?
+    var cancelable: AnyCancellable?
     @IBOutlet weak var itemsCollectionView: UICollectionView!
     @IBOutlet weak var arView: ARView!
     
@@ -39,6 +40,29 @@ class ViewController: UIViewController {
     //MARK: - Selectors
     @objc func handleTap(sender: UITapGestureRecognizer){
         let tapLocation = sender.location(in: arView)
+        let result = arView.raycast(from: tapLocation, allowing: .estimatedPlane, alignment: .horizontal)
+        
+        if let firstResult = result.first{
+            let worldPosition = simd_make_float3(firstResult.worldTransform.columns.3)
+            if let chosenItem = selectedItem{
+                cancelable = ModelEntity.loadAsync(named: chosenItem).sink(receiveCompletion: { completion in
+                    if case let .failure(error) = completion{
+                        print("Unable to load mode \(error)")
+                    }
+                }, receiveValue: { entity in
+                    //place the object at the world position
+                    self.placeObject(entity, at: worldPosition)
+                })
+            }
+            
+            
+        }
+    }
+    
+    func placeObject(_ node: Entity, at location: SIMD3<Float>){
+        let anchor = AnchorEntity(world: location)
+        anchor.addChild(node)
+        arView.scene.addAnchor(anchor)
     }
 
 }
@@ -51,6 +75,7 @@ extension ViewController: UICollectionViewDataSource{
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "item", for: indexPath) as! ItemCell
         cell.itemCellLabel.text = self.itemsArray[indexPath.row]
         cell.backgroundColor = .orange
+    
         return cell
     }
     
